cb0f20803d0905b85450d6c0b611b6a7a83ce41e
Long Wang <wangl.cc@outlook.com>
2021-11-11 19:29:58 +0800

    test: fix example "Competition"

--- test/example/competition.jl
+++ test/example/competition.jl
@@ -7,14 +7,14 @@ using EvolutionaryModelingTools
 using EvolutionaryModelingTools: sample
 
 # parameters
-clock = ContinuousClock(10.0)
+clock = ContinuousClock(100.0)
 r = 0.5   # growth rate
 K = 100   # carrying capacity
 μ = 0.01  # mutation rate
 
 # state
 v = recorded(DynamicEntry, clock, [100])
-m = recorded(DynamicEntry, clock, ones(1, 1))
+m = recorded(StaticEntry, clock, ones(1, 1))
 
 # simulate with this package
 @reaction growth begin
@@ -40,9 +40,8 @@ end
         @. c * v * m * v'
     end
     begin
-        i, j = ind.I
+        i = ind[1]
         v[i] -= 1
-        v[j] += 1
         if v[i] == 0 # check extinction
             deleteat!(v, i)
             resize!(m, (not(i), not(i)))
@@ -50,13 +49,10 @@ end
     end
 end
 
-@ufunc check_extinction(ind, v) = println(ind, v)
-
 ps = (; r, K, μ, v, m)
 rs = (growth, mutation, competition)
 
 Random.seed!(1)
-
 ps_updated = gillespie(clock, ps, rs)[1]
 
 # simulate manually
@@ -87,9 +83,8 @@ for _ in clock
         m[:, n] = rand(n)
         m[n, 1:n-1] = rand(1, n-1)
     elseif ind == 3
-        i, j = sample(competition_rate, rn - rates_sum_acc[2]).I
+        i = sample(competition_rate, rn - rates_sum_acc[2])[1]
         v[i] -= 1
-        v[j] += 1
         if v[i] == 0 # check extinction
             deleteat!(v, i)
             resize!(m, (not(i), not(i)))
@@ -101,14 +96,7 @@ end
 @test ps_updated.v == v
 @test ps_updated.m == m
 
-for (ep, em) in zip(getentries(ps.v), getentries(v))
-    @test getts(ep) == getts(em)
-    @test getvs(ep) == getvs(em)
-end
-
-for (ep, em) in zip(getentries(ps.m), getentries(m))
-    @test getts(ep) == getts(em)
-    @test getvs(ep) == getvs(em)
-end
+@test all(map(==, getentries(ps_updated.v), getentries(v)))
+@test all(map(==, values(getentries(ps_updated.m)), values(getentries(m))))
 
 end
